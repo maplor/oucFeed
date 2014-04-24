60ac2420bada189324f22e504baacf46c20dd8cd
Maplor <Maplor@163.com>
2014-04-24 20:45:15 +0800

    引入html5.js。
    引入jQuery，改写响应的脚本。
    去除无用的导航项目。

--- js/script.js
+++ js/script.js
@@ -2,27 +2,38 @@
 // IT Studio - Maplor
 // 2014-04-24
 // v1.0
-var Utils = {
-	classToggle : function(element, tclass) {
-		var classes = element.className,
-			pattern = new RegExp(tclass);
-		var hasClass = pattern.test(classes);
-		// toggle the class
-		classes = hasClass ? classes.replace(pattern, '') :
-			classes + ' ' + tclass;
-		element.className = classes.trim();
-	}
-}
-
-window.onload = function() {
-	var nav = document.getElementById('nav');
-	var collapse = document.getElementById('nav-collapse');
+//var Utils = {
+//	classToggle : function(element, tclass) {
+//		var classes = element.className,
+//			pattern = new RegExp(tclass);
+//		var hasClass = pattern.test(classes);
+//		// toggle the class
+//		classes = hasClass ? classes.replace(pattern, '') :
+//			classes + ' ' + tclass;
+//		element.className = classes.trim();
+//	}
+//}
+//
+//window.onload = function() {
+//	var nav = document.getElementById('nav');
+//	var collapse = document.getElementById('nav-collapse');
+//	
+//	Utils.classToggle(nav, 'hide');
+//	Utils.classToggle(collapse, 'active');
+//	
+//	collapse.onclick = function() {
+//		Utils.classToggle(nav, 'hide')
+//		return false;
+//	}
+//}
+$(document).ready(function() {
+	var nav = $('#nav');
+	var collapse = $('#nav-collapse');
+	nav.hide(400);
+	collapse.addClass('active');
 	
-	Utils.classToggle(nav, 'hide');
-	Utils.classToggle(collapse, 'active');
-	
-	collapse.onclick = function() {
-		Utils.classToggle(nav, 'hide')
+	collapse.on('click', function() {
+		nav.toggle(300);
 		return false;
-	}
-}
+	})
+})
