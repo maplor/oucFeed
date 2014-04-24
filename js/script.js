cf5d484b463b6229bb2b821d6d9db8efab923cb7
Maplor <Maplor@163.com>
2014-04-24 19:36:37 +0800

    去除所有原样式，调整DOM结构，实现导航栏事件。

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// JavaScript Document
+// IT Studio - Maplor
+// 2014-04-24
+// v1.0
+var Utils = {
+	classToggle : function(element, tclass) {
+		var classes = element.className,
+			pattern = new RegExp(tclass);
+		var hasClass = pattern.test(classes);
+		// toggle the class
+		classes = hasClass ? classes.replace(pattern, '') :
+			classes + ' ' + tclass;
+		element.className = classes.trim();
+	}
+}
+
+window.onload = function() {
+	var nav = document.getElementById('nav');
+	var collapse = document.getElementById('nav-collapse');
+	
+	Utils.classToggle(nav, 'hide');
+	Utils.classToggle(collapse, 'active');
+	
+	collapse.onclick = function() {
+		Utils.classToggle(nav, 'hide')
+		return false;
+	}
+}
